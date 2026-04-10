629fb284a3a0d0991498f1e87d71dd96925aa8f6
tjackr <tjackr@proton.me>
2026-04-10 17:06:38 +0200

    Initial commit. Working version with MaestroSoft logo and systems info displayed via LVGL as its own task. Much much more to come.

---
+++ main/display_handler.h
@@ -0,0 +1,25 @@
+#ifndef __ESPM_DISPLAY_HANDLER_H__
+#define __ESPM_DISPLAY_HANDLER_H__
+
+
+#define DISPLAY_SIZE_WIDTH 1024
+#define DISPLAY_SIZE_HEIGHT 600
+/* Lazy calc on 14px mono font on 1024x600 display */
+#define DISPLAY_MAX_CHAR_PER_ROW 73
+#define DISPLAY_MAX_CHAR_ROWS 42
+
+typedef struct 
+{
+
+} DH;
+
+/* ======================= INTERFACE ======================= */
+
+int dh_init(DH* _DH);
+
+void dh_work(void* _null_for_now);
+
+// void dh_dispose(DH* _DH);
+
+/* ========================================================= */
+#endif
